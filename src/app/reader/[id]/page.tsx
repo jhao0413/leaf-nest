eb9c0950c95da3d03151b49ec1ade4cc5a6ef9f6
Jhao <jhao413@qq.com>
2024-12-11 12:18:06 +0800

    🐞 fix: ReaderPageProps

--- src/app/reader/[id]/page.tsx
+++ src/app/reader/[id]/page.tsx
@@ -1,8 +1,12 @@
 import ReaderContent from "@/app/reader/[id]/ReaderContent";
+
 type ReaderPageProps = {
-  params: { id: string };
+  params: {
+    id: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export default async function ReaderPage(props: ReaderPageProps) {
-  return <ReaderContent id={props.params.id} />;
+export default async function ReaderPage({ params }: Pick<ReaderPageProps, "params">) {
+  return <ReaderContent id={params.id} />;
 }
