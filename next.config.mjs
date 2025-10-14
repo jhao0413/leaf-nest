31b2d885669549f0bc24640a93e4557ece2562c1
jhao <jhao413@qq.com>
2025-10-14 22:51:51 +0800

    config: add webpack configuration for WebAssembly support and disable source maps in production

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,32 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config, { isServer, webpack }) => {
+    if (!isServer) {
+      config.output.globalObject = 'self';
+      
+      config.experiments = {
+        ...config.experiments,
+        asyncWebAssembly: true,
+        layers: true,
+      };
+
+      config.module.rules.push({
+        test: /\.wasm$/,
+        type: 'asset/resource',
+      });
+
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+      };
+    }
+
+    return config;
+  },
+  productionBrowserSourceMaps: false
 };
 
 export default withNextIntl(nextConfig);
